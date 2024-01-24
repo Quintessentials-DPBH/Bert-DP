d8ca65fa90703d2cc3f5527a1d1139210b6d5274
abhi123-iitian <72932189+abhi123-iitian@users.noreply.github.com>
2024-01-24 12:53:23 +0530

    Delete ec-darkpattern-master directory

--- ec-darkpattern-master/darkpattern-auto-detection-classical/const/model.py
+++
@@ -1,6 +0,0 @@
-# Define label name for each model.
-
-RANDOM_FOREST_MODEL = "rfc"
-SVC_MODEL = "svm"
-LOGISTIC_REGRESSION_MODEL = "lr"
-LIGHTGBM_MODEL = "lgb"
--- ec-darkpattern-master/darkpattern-auto-detection-classical/scripts/train.sh
+++
@@ -1,3 +0,0 @@
-poetry run python $PYTHONPATH/experiments/train.py \
--m model.name=lgb,rfc,svm,lr \
-experiment.n_trial=500
